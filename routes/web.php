48740fb8b765be976a3b0ac26ea37bef4b275daf
Davide Di Palma <davidewebfullstack@gmail.com>
2023-07-27 10:55:53 +0200

    Completato

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\ArticleController;
 Route::get('/', [PublicController::class, 'welcome'])->name('welcome');
 Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
 Route::post('/article/store', [ArticleController::class, 'store'])->name('article.store');
-Route::get('article/index', [ArticleController::class, 'index'])->name('article.index');
+Route::get('article/index', [PublicController::class, 'index'])->name('article.index');
 Route::get('/article/show/{article}', [ArticleController::class, 'show'])->name('article.show');
 Route::get('/article/edit/{article}', [ArticleController::class,'edit'])->name('article.edit');
 Route::put('/article/update/{article}', [ArticleController::class, 'update'])->name('article.update');
