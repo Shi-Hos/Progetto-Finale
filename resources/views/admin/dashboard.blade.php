5923cfff648cd4cf9a43c01e4246aa578b1ee4f6
sadhos90 <sad.hossain02@gmail.com>
2023-07-31 07:46:21 +0200

    Aggiunto la vista admin dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,42 @@
+<x-layout>
+
+    <div class="container-fluid p-5 bg-info text-center text-white">
+        <div class=" row justify-content-center">
+            <h1 class="display-1">Bentornato Amministratore</h1>
+        </div>
+    </div>
+    @if (session('message'))
+    <div class="alert alert-success test-center">
+        {{ session('message') }}
+    </div>    
+    @endif
+
+    <div class="container my-5">
+        <div class=" row justify-content-center">
+            <div class="col-12">
+                <h2>Richieste per ruolo Amministratore</h2>
+                <x-requests-table :roleRequests="$adminRequests" role="amministratore"></x-requests-table>
+            </div>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <div class=" row justify-content-center">
+            <div class="col-12">
+                <h2>Richieste per ruolo Revisore</h2>
+                <x-requests-table :roleRequests="$revisoreRequests" role="revisore"></x-requests-table>
+            </div>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <div class=" row justify-content-center">
+            <div class="col-12">
+                <h2>Richieste per ruolo Redattore</h2>
+                <x-requests-table :roleRequests="$redattoreRequests" role="redattore"></x-requests-table>
+            </div>
+        </div>
+    </div>
+
+
+</x-layout>
