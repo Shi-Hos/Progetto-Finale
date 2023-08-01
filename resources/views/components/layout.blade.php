21f54bbf9a8dd9c2eb2c64205e0a6e174a87f2fc
nicolò neri <cartanerinic@gmail.com>
2023-08-01 16:32:00 +0200

    aggiunto fontawesome

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/d0364d091f.js" crossorigin="anonymous"></script>
     @vite (['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.12.2/lottie.min.js">
     <title>Fast News</title>
