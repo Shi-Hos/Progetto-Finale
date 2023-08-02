24c8cde0c3683c2431e2a5739231897a5cd722d1
Davide Di Palma <davidewebfullstack@gmail.com>
2023-08-02 15:14:08 +0200

    Error From Reso

--- resources/views/components/article-form.blade.php
+++ resources/views/components/article-form.blade.php
@@ -7,21 +7,21 @@
                     <label for="title" class="form-label">TITOLO</label>
                     <input type="text" id="title" class="form-control @error('title') is-invalid @enderror" name=title value="{{old('title')}}">
                     @error('title')
-                    <div class="alert alert-danger">{{$message}}</div>
+                    <div class="mt-1 alert alert-danger">{{$message}}</div>
                     @enderror
                 </div>
                 <div class="mb-3">
                     <label class="mt-3">SOTTOTITOLO</label>
                     <input name="subtitle" value="{{old('subtitle')}}" type="text" class="form-control @error('subtitle') is-invalid @enderror">
                     @error('subtitle')
-                    <div class="alert alert-danger">{{$message}}</div>
+                    <div class="mt-1 alert alert-danger">{{$message}}</div>
                     @enderror
                   </div>
                 <div class="mb-3">
                     <label for="body" class="form-label">DESCRIZIONE</label>
                     <textarea name="body" id="body" cols="30" rows="10" class="form-control @error('body') is-invalid @enderror">{{old('body')}}</textarea>
                     @error('body')
-                    <div class="alert alert-danger">{{$message}}</div>
+                    <div class="mt-1 alert alert-danger">{{$message}}</div>
                     @enderror
                 </div>
                 <div class="mb-3">
